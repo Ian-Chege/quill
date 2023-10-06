c2e9155b705b740b22b3d22e12c639918f420a38
Ian Chege <ianchege001@gmail.com>
2023-10-07 01:25:35 +0300

    feat: creating the Dashboard

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,9 @@
+import { useRouter, useSearchParams } from "next/navigation"
+
+const AuthCallbackPage = () => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const origin = searchParams.get("origin")
+}
+
+export default AuthCallbackPage
