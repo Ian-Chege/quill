24627e89970355f6e45e40001de33db7f80a6395
Ian Chege <ianchege001@gmail.com>
2023-10-10 12:17:49 +0300

    fix: fix deploy error

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,18 +1,35 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { publicProcedure, router } from "./trpc"
 import { TRPCError } from "@trpc/server"
+import { db } from "@/db"
 
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession()
-    const users = getUser()
+    const user = getUser()
 
-    if (!users.id || !users.email) {
+    if (!user.id || !user.email)
       throw new TRPCError({
         code: "UNAUTHORIZED",
-        message: "Unauthorized",
+      })
+
+    // check if the user is in the database
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    })
+
+    if (!dbUser) {
+      // create user in database
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
       })
     }
+
     return { success: true }
   }),
 })
